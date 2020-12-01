34e34d646069777468451ecae107c7d7b18a8af5
Yors <jatbribiesca@gmail.com>
2020-12-01 17:28:18 -0600

    fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,18 @@
 module ApplicationHelper
   def check_signed_in
-    logout = (link_to "Logout", destroy_user_session_path, method: :delete, class: "nav-link")
-    signUp = (link_to 'Sign Up', new_user_registration_path, class: "nav-link")
-    signIn = (link_to 'Sign In', new_user_session_path, class: "nav-link")
+    logout = (link_to 'Logout', destroy_user_session_path, method: :delete, class: 'nav-link')
+    sign_up = (link_to 'Sign Up', new_user_registration_path, class: 'nav-link')
+    sign_in = (link_to 'Sign In', new_user_session_path, class: 'nav-link')
     if user_signed_in?
-      content_tag(:li, content_tag(:a, 'Signed as: ' + current_user.name, href: '#', class: "nav-link"), class: "nav-item") +
-      content_tag(:li, (logout), class: "nav-item active")
+      content_tag(:li, content_tag(
+                         :a, 'Signed as: ' + current_user.name, href: '#', class: 'nav-link'
+                       ),
+                  class: 'nav-item') +
+        content_tag(:li, logout, class: 'nav-item active')
     else
-      content_tag(:li, (signIn), class: "nav-item") + 
-      content_tag(:span, '  /  ', class: "navbar-text white-text") +
-      content_tag(:li, (signUp), class: "nav-item")
+      content_tag(:li, sign_in, class: 'nav-item') +
+        content_tag(:span, '  /  ', class: 'navbar-text white-text') +
+        content_tag(:li, sign_up, class: 'nav-item')
     end
   end
 end
